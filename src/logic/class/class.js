69a0852260087b1e3e96e79f5ddc04d2941be300
lCardozo <lucascardozo036@gmail.com>
2025-09-05 16:00:57 -0300

    Actualizando codigo

--- src/logic/class/class.js
+++ src/logic/class/class.js
@@ -6,9 +6,8 @@ class user {
     #email = '';
     #telefono = '';
     #cargo = '';
-    #fechaIngreso = '';
 
-    constructor(idUsuario, dni, nombre, apellido, email, telefono, cargo, fechaIngreso) {
+    constructor(idUsuario, dni, nombre, apellido, email, telefono, cargo) {
         this.#idUsuario = idUsuario;
         this.#dni = dni;
         this.#nombre = nombre;
@@ -16,7 +15,6 @@ class user {
         this.#email = email;
         this.#telefono = telefono;
         this.#cargo = cargo;
-        this.#fechaIngreso = fechaIngreso;
     }
     modDatos(type,dato) {
         if (this.#cargo == 'admin' || this.#cargo == 'hr') {
@@ -36,12 +34,9 @@ class user {
                 case 'cargo':
                     this.#cargo = dato;
                     break;
-                case 'fechaIngreso':
-                    this.#fechaIngreso = dato;
-                    break;
-            }
+                }
         } else {
-            return 'No tienes permisos para modificar datos';
+            throw new Error('No tienes permisos para modificar datos');
             }
         }
         asignarCargo(dato){
@@ -49,7 +44,7 @@ class user {
             // ACA IRIA EL USER SELECCIONADO QUE AUN NO LO HICE ESA CLASE
             return 'Cambio realizado con exito'
             } else {
-                return 'No tienes permiso para asignar cargos'
+                throw new Error('No tienes permiso para asignar cargos')
             }
         }
         verDatos() {
@@ -60,7 +55,43 @@ class user {
                 email: this.#email,
                 telefono: this.#telefono,
                 cargo: this.#cargo,
-                fechaIngreso: this.#fechaIngreso
             };
         }
 }
+
+class asistencia {
+    #idAsistencia = '';
+    #fecha = '';
+    #horaEntrada = '';
+    #horaSalida = '';
+
+    constructor(idAsistencia, fecha, horaEntrada, horaSalida) {
+        this.#idAsistencia = idAsistencia;
+        this.#fecha = fecha;
+        this.#horaEntrada = horaEntrada;
+        this.#horaSalida = horaSalida;
+    }
+    registrarEntrada(hora) {
+        this.#horaEntrada = hora;
+        return 'Entrada registrada con exito';
+    }
+    registrarSaldida(hora) {
+        this.#horaSalida = hora;
+        return 'Salida registrada con exito';
+    }
+    generarReporte() {
+        return {
+            // Aca se mostraria el reporte de entrada y salida de los ultimo trimestre
+            fecha: this.#fecha,
+            salida: this.#horaSalida,
+            entrada: this.#horaEntrada,
+        }
+
+    }
+}
+
+const Pedro = new user(132, 45646546, "Pedro", "Juan", 'pedroj@gmail.com', '6455646465', 'admin');
+
+Pedro.modDatos('nombre','Pedras');
+console.log(Pedro.asignarCargo('empleado'));
+console.log(Pedro.verDatos());
