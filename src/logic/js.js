0109a74a0d39aa346a64a6272830dfcc911ed94f
Lauta <lautarito-alcaraz@hotmail.com>
2025-09-18 17:55:48 -0300

    cors

--- src/logic/js.js
+++ src/logic/js.js
@@ -3,7 +3,8 @@ const express = require('express');
 const mysql = require('mysql');
 const app = express();
 app.use(express.json());
-
+const cors = require('cors');
+app.use(cors());
 
 
 const connection = mysql.createConnection({
